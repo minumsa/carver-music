4210656593696f7a08abb0593bd9185c076acc19
minumsa <icycyi@naver.com>
2024-03-25 17:48:19 +0900

    정상 커밋 테스트

--- app/components/@common/Snow.tsx
+++ app/components/@common/Snow.tsx
@@ -1,6 +1,8 @@
 import Snowfall from "react-snowfall";
 import styles from "./Snow.module.css";
 
+// 정상 커밋 테스트
+
 export const Snow = () => {
   return (
     <div className={styles["snowfall-container"]}>
