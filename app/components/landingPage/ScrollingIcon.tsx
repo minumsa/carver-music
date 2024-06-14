b9399a21112757bd4f560c3d84bdeb6e91c5248e
minumsa <icycyi@naver.com>
2024-06-14 12:47:05 +0900

    refactor: 무한 스크롤 시 ScrollingIcon 1초 더 보여주는 로직 삭제

--- app/components/landingPage/ScrollingIcon.tsx
+++ app/components/landingPage/ScrollingIcon.tsx
@@ -1,8 +1,6 @@
 import SpinningCircles from "react-loading-icons/dist/esm/components/spinning-circles";
 import styles from "./LandingPage.module.css";
 import { useEffect, useState } from "react";
-import { scrollCountAtom } from "@/app/modules/atoms";
-import { useAtomValue } from "jotai";
 
 interface LoadingIconProps {
   isScrolling: boolean;
@@ -10,19 +8,14 @@ interface LoadingIconProps {
 
 export const ScrollingIcon = ({ isScrolling }: LoadingIconProps) => {
   const [displayStatus, setDisplayStatus] = useState("none");
-  const scrollCount = useAtomValue(scrollCountAtom);
-  const isFirstScroll = scrollCount < 3;
-  const showIconTime = isFirstScroll ? 1500 : 1000;
 
-  // LoadingIcon이 금방 사라져서 사용자가 인지하지 못할 경우를 대비해 1초 더 노출
   useEffect(() => {
     if (isScrolling) {
       setDisplayStatus("flex");
-      setTimeout(() => {
-        setDisplayStatus("none");
-      }, showIconTime);
+    } else {
+      setDisplayStatus("none");
     }
-  }, [isScrolling, showIconTime]);
+  }, [isScrolling]);
 
   return (
     <div style={{ display: displayStatus }}>
