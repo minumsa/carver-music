af0e8c09d7685d3e29a34f73ad5e1e59e2721d1f
minumsa <icycyi@naver.com>
2024-07-01 11:44:51 +0900

    feat: groupedCalendarDataByDate 생성

--- app/components/calendar/CalendarMonth.tsx
+++ app/components/calendar/CalendarMonth.tsx
@@ -7,7 +7,27 @@ interface CalendarMonthProps {
   calendarData: CalendarData[];
 }
 
+interface GroupedCalendarData {
+  [date: string]: {
+    album: string;
+    artist: string;
+    id: string;
+    imgUrl: string;
+  }[];
+}
+
 export const CalendarMonth = ({ calendarData }: CalendarMonthProps) => {
+  const groupedCalendarDataByDate = calendarData.reduce<GroupedCalendarData>(
+    (acc, calendarData) => {
+      const { album, artist, id, imgUrl, uploadDate } = calendarData;
+      const date = uploadDate.split("T")[0];
+      if (!acc[date]) acc[date] = [];
+      acc[date].push({ album, artist, id, imgUrl });
+      return acc;
+    },
+    {},
+  );
+
   return (
     <div className={styles.container}>
       <h3 className={styles.month}></h3>
