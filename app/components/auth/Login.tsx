74a858851c8cf3bcf442cd49a7e24c4855023eb8
minumsa <icycyi@naver.com>
2024-07-11 13:17:03 +0900

    feat: 로그인 시 이동할 path 로직 효율적으로 설정

--- app/components/auth/Login.tsx
+++ app/components/auth/Login.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { userLogin } from "@/app/modules/api";
 import styles from "./Login.module.css";
+import { getUserInfo, userLogin } from "@/app/modules/api";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+require("dotenv").config();
 
 interface LoginForm {
   userId: string;
@@ -18,16 +21,67 @@ export const Login = () => {
       password: "",
     },
   });
+  const [prevURL, setPrevURL] = useState<URL>();
+
+  const getRedirectPathForAdmin = (baseURL: string, isAdminURL: boolean, prevURL: URL) => {
+    return isAdminURL ? `${baseURL}/${prevURL.pathname}` : `${baseURL}/admin${prevURL.pathname}`;
+  };
+
+  const getRedirectPathForUser = (baseURL: string, prevURL: URL) => {
+    return `${baseURL}${prevURL.pathname}`;
+  };
+
+  const getDefaultRedirectPath = (role: string) => {
+    if (role === "admin") {
+      return "/admin";
+    } else {
+      return "/";
+    }
+  };
+
+  const handleLoginRedirect = (role: string, baseURL: string, prevURL?: URL) => {
+    if (!prevURL) return getDefaultRedirectPath(role);
+
+    const prevURLString = prevURL.toString();
+    const isBaseURLPresent = prevURLString.includes(baseURL);
+    const isAdminURL = prevURLString.includes("admin");
+
+    if (isBaseURLPresent) {
+      if (role === "admin") {
+        return getRedirectPathForAdmin(baseURL, isAdminURL, prevURL);
+      } else if (role === "user") {
+        return getRedirectPathForUser(baseURL, prevURL);
+      }
+    } else {
+      return getDefaultRedirectPath(role);
+    }
+  };
 
   const onSubmit = handleSubmit(async (data) => {
     const { userId, password } = data;
     try {
-      const response = await userLogin(userId, password);
-      if (response?.ok) router.push("/");
+      await userLogin(userId, password);
+      const response = await getUserInfo();
+      const { role } = response;
+      const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+      if (baseURL) {
+        const redirectURL: any = handleLoginRedirect(role, baseURL, prevURL);
+        router.push(redirectURL);
+      }
     } catch (error) {
       console.error(error, "Failed to sign up process");
     }
   });
+
+  useEffect(() => {
+    // 이전 페이지의 전체 URL을 가져오기
+    const referrer = document.referrer;
+
+    if (referrer) {
+      const url = new URL(referrer);
+      setPrevURL(url);
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
